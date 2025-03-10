204915b90ea4ebaa0808ed3d8fb73f82bfd843dd
gokmentas <56593104+gokmentas@users.noreply.github.com>
2025-03-10 22:03:29 +0300

    hello

---
+++ Assets/Scripts/PrintToConsole.cs
@@ -0,0 +1,17 @@
+using System;
+using UnityEngine;
+
+public class PrintToConsole : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log("Bro, It's a hard pass!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
